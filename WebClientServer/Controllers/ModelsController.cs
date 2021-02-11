229b79e8f6aaf3049e1bc186d67296a6873c74fc
VladAvdeev <vladislav.avdeev55@gmail.com>
2021-02-11 23:12:37 +0300

    change

--- WebClientServer/Controllers/ModelsController.cs
+++ WebClientServer/Controllers/ModelsController.cs
@@ -1,4 +1,6 @@
-﻿using CommonLibrary1.Models;
+﻿using CommonLibrary.Models;
+using CommonLibrary1;
+using CommonLibrary1.Models;
 using GymClientServer.Sources;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -11,45 +13,140 @@ using System.Threading.Tasks;
 
 namespace GymClientServer.Controllers
 {
-    [Route("api/employee")]
+    [Route("api")]
     [ApiController]
     public class ModelsController : ControllerBase
     {
         private readonly EmployeeRepository employeeRepository;
+        private readonly ClientRepository clientRepository;
         public ModelsController(IConfiguration configuration)
         {
             employeeRepository = new EmployeeRepository(configuration);
+            clientRepository = new ClientRepository(configuration);
         }
-        // GET: api/<ValuesController>
+        // GET: api/employeers
+        [Route("employeers")]
         [HttpGet]
-        public IEnumerable<Employee> GetAll()
+        public IEnumerable<Employee> GetAllEmp()
         {
             return employeeRepository.FindAll();
         }
-
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // Get : api/clients
+        [Route("clients")]
+        [HttpGet]
+        public IEnumerable<Client> GetAllClients()
         {
-            return "value";
+            return clientRepository.FindAll();
+        }
+        // GET api/employeers/5
+        //[Route("employeers/GetId/'{id}'")]
+        [HttpGet("employeers/{id}")]
+        public Employee GetEmpById(int id)
+        {
+            return employeeRepository.FindById(id);
+        }
+        //GET api/clients/5
+        //[Route("api/clients/{id}")]
+        [HttpGet("clients/{id}")]
+        public Client GetClientById(int id)
+        {
+            return clientRepository.FindById(id);
         }
 
-        // POST api/<ValuesController>
+        // POST api/employee
+        [Route("employee/add")]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void AddEmp(Employee employee)
         {
+            employeeRepository.Add(employee);
+        }
+        [Route("client/add")]
+        [HttpPost]
+        public void Add(Client client)
+        {
+            clientRepository.Add(client);
         }
 
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // PUT api/employee/5
+        //[Route("{api/employee/update/{id}}")]
+        [HttpPut("employee/update/{id}")]
+        public ResponseServer UpdateEmp(Employee employee)
         {
+            ResponseServer response = new ResponseServer() 
+            { 
+                Action = "Обновление данных работника", 
+                IsSuccess=false, 
+                Message = "Не удалось обновить данные работника" 
+            };
+            Employee oldEmp = employeeRepository.FindById(employee.Id);
+            if(oldEmp.Phone != employee.Phone)
+            {
+                response.IsSuccess = true;
+                employeeRepository.Update(employee);
+                response.Message = "Данные обновлены";
+            }
+            return response;
         }
-
+        [HttpPut("client/update/{id}")]
+        public ResponseServer UpdateClient(Client client)
+        {
+            ResponseServer response = new ResponseServer()
+            {
+                Action = "Обновление данных клиента",
+                IsSuccess = false,
+                Message = "Не удалось обновить данные клиента"
+            };
+            Client oldClient = new Client();
+            if (oldClient.Phone != clientRepository.FindById(client.Id).Phone)
+            {
+                response.IsSuccess = true;
+                clientRepository.Update(client);
+                response.Message = "Данные успешно обновлены";
+            }
+            return response;
+        }
         // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("employee/delete/{id}")]
+        public ResponseServer DeleteEmp(int? id)
         {
+            ResponseServer response = new ResponseServer()
+            {
+                Action = "Удаление работника из списка",
+                IsSuccess = false,
+                Message = "Не удалось удалить работника из списка"
+            };
+            if(id != null)
+            {
+                response.IsSuccess = true;
+                employeeRepository.Remove(id.Value);
+                if(employeeRepository.FindById(id.Value) == null)
+                {
+                    response.Message = "Успешно удалено";
+                }
+            }
+            return response;
+
         }
+        [HttpDelete("client/delete/{id}")]
+        public ResponseServer DeleteClient(int? id)
+        {
+            ResponseServer response = new ResponseServer()
+            {
+                Action = "Удаление клиента из списка",
+                IsSuccess = false,
+                Message = "Не удалось удалить клиента из списка"
+            };
+            if(id != null)
+            {
+                response.IsSuccess = true;
+                clientRepository.Remove(id.Value);
+                if (clientRepository.FindById(id.Value) == null)
+                {
+                    response.Message = "Успешно удалено";
+                }
+            }
+            return response;
+        }
+        
     }
 }
---
+++ WebClientServer/Sources/ClientRepository.cs
@@ -0,0 +1,77 @@
+﻿using CommonLibrary.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymClientServer.Sources
+{
+    public class ClientRepository : IRepository<Client>
+    {
+        private string tableName = "Clients";
+        private string connectionString;
+        public ClientRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetValue<string>("DBInfo:ConnectionString");
+        }
+        internal IDbConnection Connection
+        {
+            get
+            {
+                return new NpgsqlConnection(connectionString);
+            }
+        }
+        public IEnumerable<Client> FindAll()
+        {
+            using(IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Client>($"SELECT * FROM {tableName}");
+            }
+        }
+        public Client FindById(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Client>($"SELECT * FROM {tableName} WHERE Id = {id}", new { Id = id }).FirstOrDefault();
+            }
+        }
+        public void Add(Client client)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute($"INSERT INTO {tableName}(FirstName, LastName, BirthDay, Phone, Email, ClientId, GymTicketId, " +
+                    $"GymTicketPeriod, GymTicketBestBefore) " +
+                    $"VALUES(@FirstName, @LastName, @BirthDay, @Phone, @Email, @ClientId, @GymTicketId," +
+                    $"@GymTicketPeriod, @GymTicketBestBefore ", client);
+                dbConnection.Close();
+            }
+        }
+        public void Remove(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute($"DELETE FROM {tableName} WHERE Id = {id}");
+                dbConnection.Close();
+            }
+        }
+        public void Update(Client client)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Query($"UPDATE {tableName} SET FirstName = @FirstName, LastName = @LastName, BirthDay = @BirthDay, Phone = @Phone, " +
+                    $"Email = @Email, ClientId = @ClientId, GymTicketId = @GymTicketId, GymTicketPeriod = @GymTicketPeriod, GymTicketBestBefore = " +
+                    $"@GymTicketBestBefore WHERE Id = {client.Id}", client);
+                dbConnection.Close();
+            }
+        }
+    }
+}
